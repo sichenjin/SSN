65cf3303ae153d2930ce521155ad353e3c6da08f
sichenjin <hellojinsichen@gmail.com>
2022-07-25 18:54:46 -0400

    route path

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 # CORS(app) #comment this on deployment
 api = Api(app)
 
-@app.route("/", defaults={'path':''})
+@app.route("/", defaults={'path':'/flask/hello'})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
