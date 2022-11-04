7c5145a4ee8ed485d08cc02b20dc1534e86a1c64
sichenjin <hellojinsichen@gmail.com>
2022-11-04 16:18:25 -0400

    rerun flask and update outside app.py

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
-# from flask_cors import CORS #comment this on deployment
+from flask_cors import CORS #comment this on deployment
 from api.HelloApiHandler import HelloApiHandler
 from api.Community import Community
 from api.Cliques import Cliques
 from api.MyConvexHull import MyConvexHull
+from api.DensityDistance import DensityDistance
+from flask_cors import CORS, cross_origin
 
-app = Flask(__name__, static_url_path='', static_folder='argo-lite/build')
-# CORS(app) #comment this on deployment
+
+
+app = Flask(__name__, static_url_path='', static_folder='../argo-lite/build')
+CORS(app) #comment this on deployment
 api = Api(app)
 
 @app.route("/", defaults={'path':''})
+@cross_origin()
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
@@ -19,6 +24,7 @@ api.add_resource(HelloApiHandler, '/flask/hello')
 api.add_resource(Community, '/flask/community')
 api.add_resource(Cliques, '/flask/Cliques')
 api.add_resource(MyConvexHull, '/flask/convexhull')
+api.add_resource(DensityDistance, '/flask/densitydistance')
 
 if __name__=='__main__':
   app.run(debug=True)
